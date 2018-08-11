d8d0abbcb68d20b65bf2229c7078537816aec450
JasonG <jasongzj@163.com>
2018-08-11 07:30:17 +0000

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由<a href="">高高</a>模仿制作
+        </p>
+        <p class="pull-right">
+            <a href="">联系我们</a>
+        </p>
+    </div>
+</footer>
